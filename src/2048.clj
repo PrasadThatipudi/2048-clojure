850b1ba98e6c971401bdd595b2695f9ba36e6d91
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-10-15 21:39:45 +0530

    bugfix: Implements function to add consecutive pairs

--- src/2048.clj
+++ src/2048.clj
@@ -1,29 +1,36 @@
-;; (defn move-right [& numbers]
-;;   (add-pairs (pair (remove-zeros numbers))))
-
 (defn not-zero? [number]
   (not (zero? number)))
 
 (defn remove-zeros [numbers]
   (filter not-zero?  numbers))
 
-(defn make-pair [pairs, current-value]
-  (def last-pair (last pairs))
-  (cond (< (count last-pair) 2) (assoc pairs
-                                       (dec (count pairs))
-                                       (conj last-pair current-value))
-        :else (assoc pairs
-                     (count pairs)
-                     (conj [current-value]))))
+(defn make-consecutive-pair [pairs current-value]
+  (let [last-pair (last pairs)]
+    (cond (and
+           (= (count last-pair) 1)
+           (= current-value (first last-pair)))
+          (assoc pairs
+                 (dec (count pairs))
+                 (conj last-pair current-value))
 
-(defn make-pairs [numbers]
-  (reduce make-pair [[]] numbers))
+          :else (assoc pairs
+                       (count pairs)
+                       (conj [current-value])))))
+
+(defn consecutive-pairs [numbers]
+  (reduce make-consecutive-pair [] numbers))
 
 (defn add-vector [vector] (apply + vector))
 
 (defn add-pairs [pairs]
   (map add-vector  pairs))
 
-(defn move-line-left [& numbers]
-  (add-pairs (make-pairs (remove-zeros numbers))))
+(defn fill-right [vector replacement length]
+  (concat vector (repeat (- length (count vector)) replacement)))
 
+(defn move-line-left [numbers]
+  (fill-right (add-pairs (consecutive-pairs (remove-zeros numbers))) 0
+              (count numbers)))
+
+(defn move-left [table-numbers]
+  (map move-line-left table-numbers))

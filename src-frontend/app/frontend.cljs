2ac82b10f16df59dc3bc70da318ea24d9ebc9841
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2026-01-02 18:17:55 +0530

    Creates a dummy 2048 table

--- src-frontend/app/frontend.cljs
+++ src-frontend/app/frontend.cljs
@@ -2,8 +2,19 @@
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]))
 
+(defn make-col [col-value]
+  [:td {:style {:width 100 :height 100 :text-align "center"}} col-value])
+
+(defn make-row [row-values]
+  [:tr (map make-col row-values)])
+
+(defn make-board [table-data]
+  [:table {:border 1} (map make-row table-data)])
+
+(def initial-board '((0 0 0 0) (0 0 0 0) (0 0 0 0) (0 0 0 0)))
+
 (defn root []
-  [:h1 "Hello world"])
+  (make-board initial-board))
 
 (defn ^:export init []
   (rdom/render [root] (.getElementById js/document "app")))
